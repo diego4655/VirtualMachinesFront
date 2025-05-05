9c6323784511981a90c8df2a9ad8e473a4935a67
diego <dfernandoperez@gmail.com>
2025-05-05 01:38:10 -0500

    Update dependencies to Angular 18, remove unused AWS Amplify configuration, and clean up .gitignore and styles. Adjust routing to directly load the LoginComponent.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,8 +4,6 @@ import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { providePrimeNG } from 'primeng/config';
-import Aura from '@primeng/themes/aura';
 import { jwtInterceptor } from './core/interceptors/jwt.interceptor';
 
 export const appConfig: ApplicationConfig = {
@@ -13,11 +11,6 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideHttpClient(withInterceptors([jwtInterceptor])),
     ReactiveFormsModule,
-    provideAnimations(),
-    providePrimeNG({
-      theme: {
-          preset: Aura
-      }
-  })
+    provideAnimations()
   ]
 };
